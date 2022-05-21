380f46ba02a5c83f7e6d8328e83d8fd95ae814a9
imabiodun <70638812+imabiodun@users.noreply.github.com>
2022-05-21 21:01:05 +0100

    latest:     winequality-work.py

--- winequality-work.py
+++ winequality-work.py
@@ -5,25 +5,27 @@ import matplotlib.pyplot as plt
 
 filename='winequality-red.csv'
 wine_quality_profile=pd.read_csv(filename)
-print(f' here is the wine profile: {wine_quality_profile.columns}' )
-print(type(wine_quality_profile))
-# quality_less_5= wine_quality_profile['quality' ]==4.0 
-# print(wine_quality_profile[quality_less_5].round(2))
-# print('described :')
-# print(wine_quality_profile[quality_less_5].round(2).describe())
-# print("The total length is: ")
-# print(len(wine_quality_profile[quality_less_5].round(2)))
 
-# print(wine_quality_profile.describe().round(2))
-x=np.linspace(0,10,1000)
-#what's happening. why are you not responding?
 
-plt.plot(x, np.sin(x), label='sin (x)', color='r', linestyle='dotted')
-plt.plot(x, np.cos(x), color='k', linestyle='--', label='cos(x)')
-# plt.plot(x, np.log(x), color='b', linestyle='-', label='log(x)')
-plt.scatter(wine_quality_profile['citric acid'], wine_quality_profile['chlorides'])
-plt.xlabel('sin (x) and cos(x)')
-plt.ylabel('x')
-plt.title("Sine and Cosine curve")
-plt.legend()
+np_wine_quality_profile=np.array(wine_quality_profile)
+print (np_wine_quality_profile.shape)
+mask_of_chlorides=(wine_quality_profile['chlorides'].round(1))
+mask_of_quality=(wine_quality_profile['quality'])
+print(len(mask_of_chlorides))
+print(len(mask_of_quality))
+plt.style.use('ggplot')
+
+figure=plt.figure()
+ax=plt.axes()
+plt.plot(mask_of_chlorides , mask_of_quality)
+# plt.plot(x, np.sin(x), label='sin (x)', color='r', linestyle='dotted')
+# plt.plot(x, np.cos(x), color='k', linestyle='--', label='cos(x)')
+# # plt.plot(x, np.log(x), color='b', linestyle='-', label='log(x)')
+# to label the x axis of the graph
+plt.ylabel('Quality')
+# to label the y axis of the graph
+plt.xlabel('Chlorides')
+
+plt.title("Scatter Plot of Chlorides and Quality")
+# plt.legend()
 plt.show()
